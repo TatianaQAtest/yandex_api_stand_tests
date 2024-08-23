9a33b524bcf0a9ad1e4a34e6abb601321eb405f4
unknown <wholeriverinfo@gmail.com>
2024-08-23 16:14:30 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = " https://f3e77d2c-a049-4728-a5b7-1f7b4287dc7b.serverhub.praktikum-services.ru"
+CREATE_USER_PATH = "/api/v1/users/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
